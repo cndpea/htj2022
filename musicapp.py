90641026b8906375fc45c4fa02a2cd9b7c125b10
braydenyip <braydenjyip@gmail.com>
2022-01-08 18:23:32 -0500

    gets 10 random tracks from your spotify and gets the most 'dance-y' track based on a formula

--- musicapp.py
+++ musicapp.py
@@ -1,5 +1,6 @@
 import math
 import spotipy
+import random
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -13,26 +14,29 @@ def authentic_version():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id = id, client_secret = secrt, redirect_uri= 'http://localhost:8080'))
     results = sp.current_user_saved_tracks(limit=1, offset=0)
     n = results['total'] #gives number of saved tracks.
-    m = n//50 # get how many chunks of 50 we can do at once
-    r = n%50 #remainder
-    lowest_v = 2
-    for i in range(m):
+    hi_score_track = ""
+    hi_score = 0
+    for i in range(10):
         #doing 50 at a time results in much, much faster processing
-        results = sp.current_user_saved_tracks(limit=50, offset=i*50)
-        for idx, item in enumerate(results['items']):
-            track = item['track']
-            v = sp.audio_features(track['uri'])[0]['valence']
-            l = sp.audio_features(track['uri'])[0]['loudness']
-            print(idx+50*i, track['artists'][0]['name'], " – ", track['name'],  "v=", v, ", l=", l)
-    i += 1
-    for j in range(r):
-        #remainder are processed individually
-        results = sp.current_user_saved_tracks(limit=1, offset=(i+1)*50+j)
-        for idx, item in enumerate(results['items']):
-            track = item['track']
-            v = sp.audio_features(track['uri'])[0]['valence']
-            d = sp.audio_features(track['uri'])[0]['danceability']
-            print(idx + 50 * i + j, track['artists'][0]['name'], " – ", track['name'], "v=", v, ", d=", d)
+        results = sp.current_user_saved_tracks(limit=1, offset=random.randrange(0,(n-1)))
+        if (len(results) == 0):
+            break
+        track = results['items'][0]['track']
+        uri = track['uri']
+        af = sp.audio_features(uri)[0]
+        v = af['valence']
+        d = af['danceability']
+        e = af['energy']
+        score = 0.3*e + 0.6*d + 0.1*v
+
+        if (score > hi_score):
+            hi_score_track = track['uri']
+            hi_score = score
+            print(i, '*')
+        else:
+            print(i)
+    print(sp.track(hi_score_track)['artists'][0]['name'], " - ", sp.track(hi_score_track)['name'])
+    print("score= ", hi_score)
 def main():
     authentic_version()
 
