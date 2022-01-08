eedcd8b4f0a402b594fdae2b85bf806b8267c8d5
braydenyip <braydenjyip@gmail.com>
2022-01-08 00:22:22 -0500

    YOOOOOOOOOOOOO

--- musicapp.py
+++ musicapp.py
@@ -9,11 +9,11 @@ def authentic_version():
     scope = "user-library-read"
 
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id = id, client_secret = secrt, redirect_uri= 'http://localhost:8080'))
-
-    results = sp.current_user_saved_tracks()
-    for idx, item in enumerate(results['items']):
-        track = item['track']
-        print(idx, track['artists'][0]['name'], " – ", track['name'])
+    for i in range(10):
+        results = sp.current_user_saved_tracks(limit=20,offset=20*i)
+        for idx, item in enumerate(results['items']):
+            track = item['track']
+            print(20*i+idx, track['artists'][0]['name'], " – ", track['name'])
 
 def main():
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="2e7959faa85f44d6b2e859064f9dd1aa",
@@ -21,7 +21,7 @@ def main():
 
     results = sp.search(q='joji', limit=5)
     for idx, track in enumerate(results['tracks']['items']):
-        print(idx, track['name'])
+        print(idx+1, track['name'])
     print("---")
     authentic_version()
 
