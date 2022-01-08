53ec7896282ee358bf5817f492022a1af5dd8da1
braydenyip <braydenjyip@gmail.com>
2022-01-07 23:19:46 -0500

    spotipy example code

--- musicapp.py
+++ musicapp.py
@@ -1,16 +1,27 @@
-
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
-def main():
+id="2e7959faa85f44d6b2e859064f9dd1aa"
+secrt="2514bb7ab37e449ba19db71e9f43bfc2"
+def authentic_version():
 
     scope = "user-library-read"
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id = id, client_secret = secrt, redirect_uri= 'http://localhost:8080'))
 
     results = sp.current_user_saved_tracks()
     for idx, item in enumerate(results['items']):
         track = item['track']
         print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+def main():
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="2e7959faa85f44d6b2e859064f9dd1aa",
+                                                               client_secret="2514bb7ab37e449ba19db71e9f43bfc2"))
+
+    results = sp.search(q='joji', limit=20)
+    for idx, track in enumerate(results['tracks']['items']):
+        print(idx, track['name'])
+    authentic_version()
 
 main()
