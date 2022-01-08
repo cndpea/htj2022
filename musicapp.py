a35ff3a9102b323d986b1cffa6e44b7a36fc1bd3
braydenyip <braydenjyip@gmail.com>
2022-01-07 22:36:58 -0500

    spotipy example code

--- musicapp.py
+++ musicapp.py
@@ -2,11 +2,13 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-library-read"
+def main():
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    scope = "user-library-read"
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    results = sp.current_user_saved_tracks()
+    for idx, item in enumerate(results['items']):
+        track = item['track']
+        print(idx, track['artists'][0]['name'], " – ", track['name'])
