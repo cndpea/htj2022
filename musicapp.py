4196ffbafd9209b0e98e33025332abe81446ef8e
braydenyip <braydenjyip@gmail.com>
2022-01-08 21:31:42 -0500

    nice formatting, actual use of functions lol

--- musicapp.py
+++ musicapp.py
@@ -8,10 +8,8 @@ from spotipy.oauth2 import SpotifyOAuth
 id="2e7959faa85f44d6b2e859064f9dd1aa"
 secrt="2514bb7ab37e449ba19db71e9f43bfc2"
 
-def authentic_version():
+def authentic_version(sp):
 
-    scopes = ["user-library-read", "playlist-modify-public"]
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope= scopes, client_id = id, client_secret = secrt, redirect_uri= 'http://localhost:8080'))
     results = sp.current_user_saved_tracks(limit=1, offset=0)
     n = results['total'] #gives number of saved tracks.
     search_length = 20 #how many random tracks to check, keep under a hundred
@@ -33,7 +31,7 @@ def authentic_version():
         d = af['danceability']
         e = af['energy']
         #score formula
-        score = 0.4*e+0.6*d+0.6*v
+        score = 0.15*e+0.6*d+0.6*v
         #logic to figure out highest score, printout is to show program is running
         if (score > hi_score):
             hi_score_track = track['uri']
@@ -42,8 +40,11 @@ def authentic_version():
         else:
             print(i)
     print(sp.track(hi_score_track)['artists'][0]['name'], " - ", sp.track(hi_score_track)['name'])
-    print("score= ", hi_score)
+    print("score= {0:.3f}".format(hi_score))
+
 def main():
-    authentic_version()
+    scopes = ["user-library-read", "playlist-modify-public"]
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope= scopes, client_id = id, client_secret = secrt, redirect_uri= 'http://localhost:8080'))
+    authentic_version(sp)
 
 main()
