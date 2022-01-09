241dec302f208453ca3740bd653cdc1f725f387a
Karina Garmendez <kgarmend@uoguelph.ca>
2022-01-08 19:16:37 -0500

    brayden's additions to musicapp.py  & webapp.py port change

--- webapp.py
+++ webapp.py
@@ -6,4 +6,4 @@ app.register_blueprint(routes, url_prefix="/htj") # htj = Hack the Job, can alwa
 app.secret_key = '8f93wn_febiw28983bf3f3983/'
 
 if __name__ == '__main__':
-    app.run(debug = True, port=8080)
+    app.run(debug = True, port=8000)
